35be0b3700c36ea1a5604a0e4a69ea1099287b19
Rocio <rocio95cua>
2019-05-06 22:30:30 -0500

    read  sound example

--- Firmware/read_sound/read_sound.ino
+++ Firmware/read_sound/read_sound.ino
@@ -12,24 +12,78 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 #define Serial SerialUSB
+#define N 5
 
-//read microphone
-float deci=0;
+float filtered;
+float vals[N];
 
-void setup() {
-  //resolution analog reads
-  analogReadResolution(12);
-  Serial.begin(9600);
+const double dBAnalogQuiet = 8; // envelope - calibrated value from analog input (48 dB equivalent)
+const double dBAnalogModerate = 14;
+const double dBAnalogLoud = 30;
+int count;
+
+float calculateDecibels(int x, char c)
+{
+  float decibelsCalculated;
+  
+    if (c == 'q')
+      decibelsCalculated = 20 * log10(x/dBAnalogQuiet);
+    if (c == 'm')
+      decibelsCalculated = 20 * log10(x/dBAnalogModerate);
+    if (c == 'l')
+      decibelsCalculated = 20 * log10(x/dBAnalogLoud);  
+  
+  return (decibelsCalculated);
 }
 
-void loop() {
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  int value=0, i;
+  float decibelsValueQuiet = 49;//49.5;//float decibelsValueQuiet = 49.5;
+  float decibelsValueModerate = 65;//65;
+  float decibelsValueLoud = 85;
+  float valueDb;
+  float sum=0;
   
-  //Get Sensor Data
-  deci=analogRead(A0);
+  value = analogRead(A0);
   
-  Serial.print("SOUND");
-  Serial.print(" ");
-  Serial.println(deci);
- 
-  delay(1000);
+  if (value <= 15)
+  {
+    decibelsValueQuiet += calculateDecibels(value, 'q');
+    valueDb = decibelsValueQuiet; 
+  }
+  else if ((value > 15) && ( value < 23) )
+  {
+    decibelsValueModerate += calculateDecibels(value, 'm');
+    valueDb = decibelsValueModerate;
+  }
+  else if(value >= 23)
+  {
+    decibelsValueLoud += calculateDecibels(value, 'l');
+    valueDb = decibelsValueLoud;
+  }
+
+  for (int i = N-1 ; i>0 ; i--)
+  {
+    vals[i]=vals[i-1];  
+  }
+  vals[0]=valueDb;
+  
+  for(int i=0 ; i<N ; i++)
+  {
+    sum=sum+vals[i];
+  }
+  filtered = sum/N;
+  
+  Serial.print(value);
+  Serial.print("\t");
+  Serial.print(valueDb);
+  Serial.print("\t");
+  Serial.println(filtered);
+  delay(200);
 }
