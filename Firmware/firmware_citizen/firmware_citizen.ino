0979661955e59240cae9ad10b96dbba1cfabf8e4
Sabas <s@electroniccats.com>
2020-02-22 16:30:49 -0600

    Fix Index UV

--- Firmware/firmware_citizen/firmware_citizen.ino
+++ Firmware/firmware_citizen/firmware_citizen.ino
@@ -1,9 +1,8 @@
 /*******************************************************************************
   Created by: Eduardo Contreras @ Electronic Cats 2019
               Rocio Rodriguez @ Electronic Cats 2019
-  Based on Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
-  PLEASE REFER TO THIS LMIC LIBRARY https://github.com/things-nyc/arduino-lmic
-  
+              Andres Sabas @ Electronic Cats 2020
+ 
   Example for the CITIZEN board
     In this example you can send:
     TEMPERATURE, HUMINIDY AND PRESSURE sensor information read by the BME280 
@@ -38,8 +37,29 @@
 
 #ifdef _USE_VME_ 
   VEML6075 uv; 
-  String UVi;
-  float iUV=0;
+  // Calibration constants:
+  // Four gain calibration constants -- alpha, beta, gamma, delta -- can be used to correct the output in
+  // reference to a GOLDEN sample. The golden sample should be calibrated under a solar simulator.
+  // Setting these to 1.0 essentialy eliminates the "golden"-sample calibration
+  const float CALIBRATION_ALPHA_VIS = 1.0; // UVA / UVAgolden
+  const float CALIBRATION_BETA_VIS  = 1.0; // UVB / UVBgolden
+  const float CALIBRATION_GAMMA_IR  = 1.0; // UVcomp1 / UVcomp1golden
+  const float CALIBRATION_DELTA_IR  = 1.0; // UVcomp2 / UVcomp2golden
+
+  // Responsivity:
+  // Responsivity converts a raw 16-bit UVA/UVB reading to a relative irradiance (W/m^2).
+  // These values will need to be adjusted as either integration time or dynamic settings are modififed.
+  // These values are recommended by the "Designing the VEML6075 into an application" app note for 100ms IT
+  const float UVA_RESPONSIVITY = 0.00110; // UVAresponsivity
+  const float UVB_RESPONSIVITY = 0.00125; // UVBresponsivity
+
+  // UV coefficients:
+  // These coefficients
+  // These values are recommended by the "Designing the VEML6075 into an application" app note
+  const float UVA_VIS_COEF_A = 2.22; // a
+  const float UVA_IR_COEF_B  = 1.33; // b
+  const float UVB_VIS_COEF_C = 2.95; // c
+  const float UVB_IR_COEF_D  = 1.75; // d
 #endif
 
 #ifdef _USE_SOUND_ 
@@ -137,6 +157,11 @@ void setup() {
     Serial.println("Unable to communicate with VEML6075.");
     while (1);
   }
+
+  // Integration time and high-dynamic values will change the UVA/UVB sensitivity. That means
+  // new responsivity values will need to be measured for every combination of these settings.
+  uv.setIntegrationTime(VEML6075::IT_100MS);
+  uv.setHighDynamic(VEML6075::DYNAMIC_NORMAL);
   #endif
 
   // Set LoRaWAN Class change CLASS_A or CLASS_C
@@ -202,10 +227,9 @@ void getInfoAndSend() {
   #endif
 
     #ifdef _USE_VME_  //Temperature/HUMIDITY/PRESSURE
-    float  UV= readUV();
     Serial.print("[INFO] UV indice:"); 
-    Serial.println(UV,2);
-    lpp.addAnalogInput(chan++,UV);
+    Serial.println(readUV(),2);
+    lpp.addAnalogInput(chan++,readUV());
   #endif
 
    #ifdef _USE_SOUND_  //Temperature/HUMIDITY/PRESSURE
@@ -289,9 +313,28 @@ float readCO2(void) {
 
 #ifdef _USE_VME_  
 float readUV(void) {
-  UVi=uv.index();
-  iUV=UVi.toInt();
-  return iUV;
+  uint16_t rawA, rawB, visibleComp, irComp;
+  float uviaCalc, uvibCalc, uvia, uvib, uvi;
+
+  // Read raw and compensation data from the sensor
+  rawA = uv.rawUva();
+  rawB = uv.rawUvb();
+  visibleComp = uv.visibleCompensation();
+  irComp = uv.irCompensation();
+
+  // Calculate the simple UVIA and UVIB. These are used to calculate the UVI signal.
+  uviaCalc = (float)rawA - ((UVA_VIS_COEF_A * CALIBRATION_ALPHA_VIS * visibleComp) / CALIBRATION_GAMMA_IR)
+                  - ((UVA_IR_COEF_B  * CALIBRATION_ALPHA_VIS * irComp) /  CALIBRATION_DELTA_IR);
+  uvibCalc = (float)rawB - ((UVB_VIS_COEF_C * CALIBRATION_BETA_VIS * visibleComp) / CALIBRATION_GAMMA_IR)
+                  - ((UVB_IR_COEF_D  * CALIBRATION_BETA_VIS * irComp) /  CALIBRATION_DELTA_IR);
+
+  // Convert raw UVIA and UVIB to values scaled by the sensor responsivity
+  uvia = uviaCalc * (1.0 / CALIBRATION_ALPHA_VIS) * UVA_RESPONSIVITY;
+  uvib = uvibCalc * (1.0 / CALIBRATION_BETA_VIS) * UVB_RESPONSIVITY;
+
+  // Use UVIA and UVIB to calculate the average UVI:
+  uvi = (uvia + uvib) / 2.0;
+  return uvi;
 }
 #endif
 
